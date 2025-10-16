7282412daf0acb94433b2cceb42b93d85bbd0e22
ewbriao1978 <eduardo.briao@gmail.com>
2025-10-16 14:08:35 -0300

    CORS

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+// permitir requisições de outros domínios
+// (necessário para front-end e back-end em servidores diferentes)
 // criar rotas
 const funcRoutes = require('./routes/FuncionariosRoutes');
 const app = express();
 const port = 3000;
 // para permitir o uso de json no body das reqs.
 app.use(express.json());
+app.use(cors());
 app.use(funcRoutes);
 
 app.listen(port, () => {
